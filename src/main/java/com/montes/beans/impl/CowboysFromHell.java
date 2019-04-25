05f818073f721a6fbfd2b6aa858f1b64e852e783
thrasht <eduardo.mhdz@gmail.com>
2019-04-24 19:59:48 -0500

    solving abiguity with @Primary annotation

--- src/main/java/com/montes/beans/impl/CowboysFromHell.java
+++ src/main/java/com/montes/beans/impl/CowboysFromHell.java
@@ -1,10 +1,12 @@
 package com.montes.beans.impl;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.montes.beans.CompactDisk;
 
-
+@Component
+@Primary
 public class CowboysFromHell implements CompactDisk{
 
 	private String title = "Cowboys From Hell";
